34dc0105eace8da751a0a529c7014560b1afdd92
gengjiawen <technicalcute@gmail.com>
2021-10-04 13:51:26 +0800

    feat: add console hook for wasm

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -3,6 +3,7 @@ mod utils;
 use wasm_bindgen::prelude::*;
 use parser::{parse_ast_json_string};
 use wasm_bindgen::throw_str;
+use crate::utils::set_panic_hook;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -12,6 +13,7 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
 pub fn parse(input: &str) -> String {
+    set_panic_hook();
     match parse_ast_json_string(input) {
         Ok(node) => {
             node.to_string()
