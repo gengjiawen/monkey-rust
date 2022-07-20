5ddbc95156a91def6a27f7459142968627acf982
gengjiawen <technicalcute@gmail.com>
2022-07-20 23:37:27 +0800

    fix: prod build

--- packages/playground/vite.config.ts
+++ packages/playground/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import wasm from "vite-plugin-wasm";
+import topLevelAwait from "vite-plugin-top-level-await"
+import wasm from "vite-plugin-wasm"
 
 
 // https://vitejs.dev/config/
@@ -11,5 +12,6 @@ export default defineConfig({
   plugins: [
     react(),
     wasm(),
+    topLevelAwait()
   ],
 })
