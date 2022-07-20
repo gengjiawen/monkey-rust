7cd371aa1f507210726abb3fc29780e3b355708f
gengjiawen <technicalcute@gmail.com>
2022-07-20 15:51:19 +0800

    feat: add compiler to wasm

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,9 +1,12 @@
 mod utils;
 
 use crate::utils::set_panic_hook;
+use parser::parse as parser_pase;
 use parser::parse_ast_json_string;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::throw_str;
+use compiler::compiler::Compiler;
+use parser::ast::Node;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -17,5 +20,22 @@ pub fn parse(input: &str) -> String {
     match parse_ast_json_string(input) {
         Ok(node) => node.to_string(),
         Err(e) => throw_str(format!("parse error: {}", e[0]).as_str()),
+    }
+}
+
+#[wasm_bindgen]
+pub fn compile(input: &str) -> String {
+    set_panic_hook();
+
+    let program = match parser_pase(input) {
+        Ok(ast) => ast,
+        Err(e) => throw_str(format!("parse error: {}", e[0]).as_str()),
+    };
+    let mut compiler = Compiler::new();
+    match  compiler.compile(&program) {
+        Ok(bytecode) => {
+            return bytecode.instructions.string()
+        },
+        Err(e) => throw_str(format!("compile error: {}", e).as_str()),
     }
 }
