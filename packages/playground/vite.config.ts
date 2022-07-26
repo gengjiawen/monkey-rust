fcd7a382c0a3edcf89a3102dd3404242521ca067
gengjiawen <technicalcute@gmail.com>
2022-07-27 00:03:36 +0800

    fix: gh deploy with vite

--- packages/playground/vite.config.ts
+++ packages/playground/vite.config.ts
@@ -6,6 +6,7 @@ import wasm from "vite-plugin-wasm"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_ACTION ? '/monkey-rust/' : '/',
   server: {
     port: 3000
   },
