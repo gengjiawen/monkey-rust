873e2adfa265564b6fe0760f1d293c3508b354c8
Jiawen Geng <technicalcute@gmail.com>
2023-07-17 14:55:19 +0000

    fix: script update

--- scripts/bump_cargo_packages.ts
+++ scripts/bump_cargo_packages.ts
@@ -4,6 +4,7 @@ const pak = require('../package.json')
 console.log(pak)
 
 execSync(`cargo install cargo-workspaces`)
+console.log(`install rust deps done`)
 const bump_cmd = `cargo workspaces version custom ${pak.version} --no-git-commit -y`
 console.log(bump_cmd)
 execSync(bump_cmd)
